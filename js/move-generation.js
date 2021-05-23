6ba41f07a2baa0fdef46f732e800f03997c0f4de
Shant <prashantghimire79@gmail.com>
2021-05-23 20:07:02 +0545

    seperate move generator function into smaller functions

--- js/move-generation.js
+++ js/move-generation.js
@@ -8,19 +8,16 @@ class MoveGeneration {
     static addCaptureMove(move, chessBoard) {
         chessBoard.moveList[chessBoard.moveListStart[chessBoard.ply + 1]] = move;
         chessBoard.moveScore[chessBoard.moveListStart[chessBoard.ply + 1]++] = 0;
-        // chessBoard.moveListStart[chessBoard.ply + 1];
     }
 
     static addQuietMove(move, chessBoard) {
         chessBoard.moveList[chessBoard.moveListStart[chessBoard.ply + 1]] = move;
         chessBoard.moveScore[chessBoard.moveListStart[chessBoard.ply + 1]++] = 0;
-        // chessBoard.moveListStart[chessBoard.ply + 1]++;
     }
 
     static addEnPassantMove(move, chessBoard) {
         chessBoard.moveList[chessBoard.moveListStart[chessBoard.ply + 1]] = move;
         chessBoard.moveScore[chessBoard.moveListStart[chessBoard.ply + 1]++] = 0;
-        // chessBoard.moveListStart[chessBoard.ply + 1]++;
     }
 
     static addWhitePawnCaptureMove(from, to, capture, chessBoard) {
@@ -75,146 +72,111 @@ class MoveGeneration {
     static generateMoves(chessBoard) {
         chessBoard.moveListStart[chessBoard.ply + 1] = chessBoard.moveListStart[chessBoard.ply];
 
-        let pieceType;
-        let pceIndex;
+        if (chessBoard.side == COLOR.WHITE) {
+            this.MoveGenerationWhitePawn(chessBoard)
+
+            this.MoveGenerationWhiteKingsideCastleling(chessBoard)
+            this.MoveGenerationWhiteQueenSideCastleling(chessBoard)
+
+        } else {
+            this.MoveGenerationBlackPawn(chessBoard)
+
+            this.MoveGenerationBlackKingSideCastleling(chessBoard);
+            this.MoveGenerationBlackQueenSideCastleling(chessBoard);
+        }
+
+        this.MoveGenerationNonSlidingPieces(chessBoard);
+        this.MoveGenerationSlidingPieces(chessBoard);
+    }
+
+    static MoveGenerationWhitePawn(chessBoard) {
+        let pieceNumber;
+        let square;
+
+        let pieceType = PIECES.WHITE_PAWN;
+        for (pieceNumber = 0; pieceNumber < chessBoard.piecesNumber[pieceType]; ++pieceNumber) {
+            square = chessBoard.piecesList[pieceIndex(pieceType, pieceNumber)];
+
+            if (chessBoard.pieces[square + 10] === PIECES.EMPTY) {
+                this.addWhitePawnQuietMove(square, square + 10, chessBoard);
+                if (RANKS_BOARD[square] === RANKS.RANK_2 && chessBoard.pieces[square + 20] == PIECES.EMPTY) {
+                    this.addQuietMove(this.move(square, square + 20, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_PAWN_START), chessBoard);
+
+                }
+            }
+
+            if (squareOffBoard(square + 9) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square + 9]] == COLOR.BLACK) {
+                this.addWhitePawnCapture(square, square + 9, chessBoard.pieces[square + 9]);
+            }
+
+
+
+            if (squareOffBoard(square + 11) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square + 11]] == COLOR.BLACK) {
+                this.addwhitePawnCapture(square, square + 11, chessBoard.pieces[square + 11]);
+
+            }
+
+            if (chessBoard.enPassent !== SQUARES.NO_SQ) {
+                if (square + 9 === chessBoard.enPassent) {
+                    this.addEnPassantMove(this.move(square, square + 9, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
+                }
+
+                if (square + 11 === chessBoard.enPassent) {
+                    this.addEnPassantMove(this.move(square, square + 11, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
+                }
+            }
+        }
+    }
+
+    static MoveGenerationBlackPawn(chessBoard) {
+        let pieceNumber;
+        let square;
+
+        pieceType = PIECES.BLACK_PAWN;
+
+        for (pieceNumber = 0; pieceNumber < chessBoard.pieceNumber; pieceNumber++) {
+            square = chessBoard.pieceList[pieceIndex(pieceType, pieceNumber)];
+
+            if (chessBoard.pieces[square - 10] === PIECES.EMPTY) {
+                this.addWhitePawnQuietMove(square, square - 10)
+                if (RANKS_BOARD[square] === RANKS.RANK_7 && chessBoard.pieces[square - 20] == PIECES.EMPTY) {
+                    this.addQuietMove(this.move(square, square - 20, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_PAWN_START), chessBoard);
+                }
+            }
+
+            if (squareOffBoard(square - 9) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square - 9]] == COLOR.WHITE) {
+                this.addBlackPawnCapture(square, square - 9, chessBoard.pieces[square - 9]);
+
+            }
+
+            if (squareOffBoard(square - 11) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square - 11]] == COLOR.WHITE) {
+                this.addBlackPawnCapture(square, square - 11, chessBoard.pieces[square - 11]);
+
+            }
+
+            if (chessBoard.enPas !== SQUARE.NO_SQ) {
+                if (square - 9 === chessBoard.enPas) {
+                    this.addQuietMove(this.move(square, square - 9, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
+                }
+
+                if (square - 11 === chessBoard.enPas) {
+                    this.addQuietMove(this.move(square, square - 11, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
+                }
+            }
+        }
+
+    }
+
+    static MoveGenerationNonSlidingPieces(chessBoard) {
+
         let pieceNumber;
         let target_square;
-        let piece;
         let square;
         let direction;
         let index;
 
-
-        if (chessBoard.side == COLOR.WHITE) {
-            pieceType = PIECES.WHITE_PAWN;
-            for (pieceNumber = 0; pieceNumber < chessBoard.piecesNumber[pieceType]; ++pieceNumber) {
-                square = chessBoard.piecesList[pieceIndex(pieceType, pieceNumber)];
-
-                if (chessBoard.pieces[square + 10] === PIECES.EMPTY) {
-                    this.addWhitePawnQuietMove(square, square + 10, chessBoard);
-                    if (RANKS_BOARD[square] === RANKS.RANK_2 && chessBoard.pieces[square + 20] == PIECES.EMPTY) {
-                        this.addQuietMove(this.move(square, square + 20, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_PAWN_START), chessBoard);
-
-                    }
-                }
-
-                if (squareOffBoard(square + 9) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square + 9]] == COLOR.BLACK) {
-                    this.addWhitePawnCapture(square, square + 9, chessBoard.pieces[square + 9]);
-                }
-
-
-
-                if (squareOffBoard(square + 11) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square + 11]] == COLOR.BLACK) {
-                    this.addwhitePawnCapture(square, square + 11, chessBoard.pieces[square + 11]);
-
-                }
-
-                if (chessBoard.enPassent !== SQUARES.NO_SQ) {
-                    if (square + 9 === chessBoard.enPassent) {
-                        this.addEnPassantMove(this.move(square, square + 9, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
-                    }
-
-                    if (square + 11 === chessBoard.enPassent) {
-                        this.addEnPassantMove(this.move(square, square + 11, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
-                    }
-                }
-            }
-
-            if (chessBoard.castlePermission & CASTLEBIT.WHITE_KING_SIDE_CASTLE) {
-
-                if (chessBoard.pieces[SQUARES.F1] == PIECES.EMPTY &&
-                    chessBoard.pieces[SQUARES.G1] == PIECES.EMPTY) {
-
-                    if (chessBoard.squareAttacked(SQUARES.F1, COLOR.WHITE) == BOOL.FALSE &&
-                        chessBoard.squareAttacked(SQUARES.E1, COLOR.WHITE) == BOOL.FALSE) {
-                        this.addQuietMove(this.move(SQUARES.E1, SQUARES.G1, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
-
-                    }
-                }
-            }
-
-            if (chessBoard.castlePermission & CASTLEBIT.WHITE_QUEEN_SIDE_CASTLE) {
-
-                if (chessBoard.pieces[SQUARES.D1] == PIECES.EMPTY &&
-                    chessBoard.pieces[SQUARES.C1] == PIECES.EMPTY &&
-                    chessBoard.pieces[SQUARES.b1] == PIECES.EMPTY) {
-
-                    if (chessBoard.squareAttacked(SQUARES.D1, COLOR.WHITE) == BOOL.FALSE &&
-                        chessBoard.squareAttacked(SQUARES.E1, COLOR.WHITE) == BOOL.FALSE) {
-                        this.addQuietMove(this.move(SQUARES.E1, SQUARES.C1, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
-
-                    }
-                }
-            }
-
-        } else {
-
-            pieceType = PIECES.BLACK_PAWN;
-
-            for (pieceNumber = 0; pieceNumber < chessBoard.pieceNumber; pieceNumber++) {
-                square = chessBoard.pieceList[pieceIndex(pieceType, pieceNumber)];
-
-                if (chessBoard.pieces[square - 10] === PIECES.EMPTY) {
-                    this.addWhitePawnQuietMove(square, square - 10)
-                    if (RANKS_BOARD[square] === RANKS.RANK_7 && chessBoard.pieces[square - 20] == PIECES.EMPTY) {
-                        this.addQuietMove(this.move(square, square - 20, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_PAWN_START), chessBoard);
-                    }
-                }
-
-                if (squareOffBoard(square - 9) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square - 9]] == COLOR.WHITE) {
-                    this.addBlackPawnCapture(square, square - 9, chessBoard.pieces[square - 9]);
-
-                }
-
-                if (squareOffBoard(square - 11) == BOOL.FALSE && PIECE_COLOR[chessBoard.pieces[square - 11]] == COLOR.WHITE) {
-                    this.addBlackPawnCapture(square, square - 11, chessBoard.pieces[square - 11]);
-
-                }
-
-                if (chessBoard.enPas !== SQUARE.NO_SQ) {
-                    if (square - 9 === chessBoard.enPas) {
-                        this.addQuietMove(this.move(square, square - 9, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
-                    }
-
-                    if (square - 11 === chessBoard.enPas) {
-                        this.addQuietMove(this.move(square, square - 11, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_EN_PASSANT), chessBoard);
-                    }
-                }
-            }
-
-            if (chessBoard.castlePermission & CASTLEBIT.BLACK_KING_SIDE_CASTLE) {
-
-                if (chessBoard.pieces[SQUARES.F8] == PIECES.EMPTY &&
-                    chessBoard.pieces[SQUARES.G8] == PIECES.EMPTY) {
-
-                    if (chessBoard.squareAttacked(SQUARES.F8, COLOR.BLACK) == BOOL.FALSE &&
-                        chessBoard.squareAttacked(SQUARES.E8, COLOR.BLACK) == BOOL.FALSE) {
-                        this.addQuietMove(this.move(SQUARES.E8, SQUARES.G8, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
-
-                    }
-                }
-            }
-
-            if (chessBoard.castlePermission & CASTLEBIT.BLACK_QUEEN_SIDE_CASTLE) {
-
-                if (chessBoard.pieces[SQUARES.D8] == PIECES.EMPTY &&
-                    chessBoard.pieces[SQUARES.C8] == PIECES.EMPTY &&
-                    chessBoard.pieces[SQUARES.b8] == PIECES.EMPTY) {
-
-                    if (chessBoard.squareAttacked(SQUARES.D8, COLOR.BLACK) == BOOL.FALSE &&
-                        chessBoard.squareAttacked(SQUARES.E8, COLOR.BLACK) == BOOL.FALSE) {
-                        this.addQuietMove(this.move(SQUARES.E8, SQUARES.C8, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
-
-
-                    }
-                }
-            }
-
-
-        }
-
-        pceIndex = LOOP_NON_SLIDE_INDEX[chessBoard.side];
-        piece = LOOP_NON_SLIDE_PIECE[pceIndex++];
+        let pceIndex = LOOP_NON_SLIDE_INDEX[chessBoard.side];
+        let piece = LOOP_NON_SLIDE_PIECE[pceIndex++];
 
         while (piece !== 0) {
             for (pieceNumber = 0; pieceNumber < chessBoard.piecesNumber[piece]; pieceNumber++) {
@@ -241,9 +203,18 @@ class MoveGeneration {
                 piece = LOOP_NON_SLIDE_PIECE[pceIndex++]
             }
         }
+    }
 
-        pceIndex = LOOP_SLIDE_PIECE_INDEX[chessBoard.side];
-        piece = LOOP_SLIDE_PIECE[pceIndex++];
+    static MoveGenerationSlidingPieces(chessBoard) {
+
+        let pieceNumber;
+        let target_square;
+        let square;
+        let direction;
+        let index;
+
+        let pceIndex = LOOP_SLIDE_PIECE_INDEX[chessBoard.side];
+        let piece = LOOP_SLIDE_PIECE[pceIndex++];
 
         while (piece !== 0) {
             for (pieceNumber = 0; pieceNumber < chessBoard.piecesNumber[piece]; pieceNumber++) {
@@ -270,6 +241,69 @@ class MoveGeneration {
 
                 }
                 piece = LOOP_SLIDE_PIECE[pceIndex++]
+            }
+        }
+    }
+
+    static MoveGenerationWhiteKingsideCastleling(chessBoard) {
+        if (chessBoard.castlePermission & CASTLEBIT.WHITE_KING_SIDE_CASTLE) {
+
+            if (chessBoard.pieces[SQUARES.F1] == PIECES.EMPTY &&
+                chessBoard.pieces[SQUARES.G1] == PIECES.EMPTY) {
+
+                if (chessBoard.squareAttacked(SQUARES.F1, COLOR.WHITE) == BOOL.FALSE &&
+                    chessBoard.squareAttacked(SQUARES.E1, COLOR.WHITE) == BOOL.FALSE) {
+                    this.addQuietMove(this.move(SQUARES.E1, SQUARES.G1, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
+
+                }
+            }
+        }
+    }
+
+    static MoveGenerationWhiteQueenSideCastleling(chessBoard) {
+        if (chessBoard.castlePermission & CASTLEBIT.WHITE_QUEEN_SIDE_CASTLE) {
+
+            if (chessBoard.pieces[SQUARES.D1] == PIECES.EMPTY &&
+                chessBoard.pieces[SQUARES.C1] == PIECES.EMPTY &&
+                chessBoard.pieces[SQUARES.b1] == PIECES.EMPTY) {
+
+                if (chessBoard.squareAttacked(SQUARES.D1, COLOR.WHITE) == BOOL.FALSE &&
+                    chessBoard.squareAttacked(SQUARES.E1, COLOR.WHITE) == BOOL.FALSE) {
+                    this.addQuietMove(this.move(SQUARES.E1, SQUARES.C1, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
+
+                }
+            }
+        }
+    }
+
+    static MoveGenerationBlackKingsideCastleling(chessBoard) {
+        if (chessBoard.castlePermission & CASTLEBIT.BLACK_KING_SIDE_CASTLE) {
+
+            if (chessBoard.pieces[SQUARES.F8] == PIECES.EMPTY &&
+                chessBoard.pieces[SQUARES.G8] == PIECES.EMPTY) {
+
+                if (chessBoard.squareAttacked(SQUARES.F8, COLOR.BLACK) == BOOL.FALSE &&
+                    chessBoard.squareAttacked(SQUARES.E8, COLOR.BLACK) == BOOL.FALSE) {
+                    this.addQuietMove(this.move(SQUARES.E8, SQUARES.G8, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
+
+                }
+            }
+        }
+    }
+
+    static MoveGenerationBlackQueenSideCastleling(chessBoard) {
+        if (chessBoard.castlePermission & CASTLEBIT.BLACK_QUEEN_SIDE_CASTLE) {
+
+            if (chessBoard.pieces[SQUARES.D8] == PIECES.EMPTY &&
+                chessBoard.pieces[SQUARES.C8] == PIECES.EMPTY &&
+                chessBoard.pieces[SQUARES.b8] == PIECES.EMPTY) {
+
+                if (chessBoard.squareAttacked(SQUARES.D8, COLOR.BLACK) == BOOL.FALSE &&
+                    chessBoard.squareAttacked(SQUARES.E8, COLOR.BLACK) == BOOL.FALSE) {
+                    this.addQuietMove(this.move(SQUARES.E8, SQUARES.C8, PIECES.EMPTY, PIECES.EMPTY, MOVE_FLAG_CASTLEING), chessBoard);
+
+
+                }
             }
         }
     }
